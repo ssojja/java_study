4ffaf21ee509f0f3402ea8a469dffefed11cd5ae
kim <ssojja0987@gmail.com>
2025-06-24 14:06:58 +0900

    Initial Commit

---
+++ src/chapter02/HelloJava.java
@@ -0,0 +1,10 @@
+package chapter02;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to JAVA World ~!");
+	}
+
+}
