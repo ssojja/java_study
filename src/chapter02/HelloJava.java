0c36ec550516652bbec998cc4ce85baa1edbb480
kim <ssojja0987@gmail.com>
2025-06-24 14:23:26 +0900

    .md file modified commit

--- src/chapter02/HelloJava.java
+++ src/chapter02/HelloJava.java
@@ -1,9 +1,10 @@
 package chapter02;
 
 public class HelloJava {
-
+	// class명 첫 글자 무조건 대문자!
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 메인 메소드는 실행하는데에 필수인 메소드
 		System.out.println("Welcome to JAVA World ~!");
 	}
 
