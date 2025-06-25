df1e8035c094969af3d4caecd3efcda00688083c
kim <ssojja0987@gmail.com>
2025-06-25 11:42:52 +0900

    Initial commit

---
+++ src/chapter03/Variable.java
@@ -0,0 +1,14 @@
+package chapter03;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// 기본 자료형 변수 정의
+		int age = 10;
+		double aged = 10.0;
+		boolean flag = true; // true(1), false(0)
+		char name = '홍';
+
+	}
+
+}
