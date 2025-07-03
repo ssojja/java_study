a528bcb257d638a29054a202a87e4dcca191ef60
kim <ssojja0987@gmail.com>
2025-07-03 18:16:08 +0900

    7.3 study commit

--- src/chapter07/ScoreMgmSystem.java
+++ src/chapter07/ScoreMgmSystem.java
@@ -2,6 +2,11 @@ package chapter07;
 
 import java.util.Scanner;
 
+/*
+ * - 학생 3명의 점수를 입력받는다.
+ * - 한 명 입력 후 계속 입력 여부를 메시지로 출력 후 받는다.
+ * 
+ * */
 public class ScoreMgmSystem {
 	// Field
 	String admin;
@@ -12,7 +17,7 @@ public class ScoreMgmSystem {
 	// Constructor
 	public ScoreMgmSystem() {}
 	
-	// Method 등록(insert)/검색(search)/수정(update)/삭제(remove)
+	// Method 등록(insert), 검색(search), 수정(update), 삭제(remove)
 	public void insert() {	
 		System.out.println("-- insert --");
 		student = new Student();
